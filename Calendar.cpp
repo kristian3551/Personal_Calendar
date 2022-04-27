d468b6d2699717baeae1563453219927d70a0a10
Kristian Todorov <63777932+kristian3551@users.noreply.github.com>
2022-04-27 15:19:22 +0300

    added files func

--- Calendar.cpp
+++ Calendar.cpp
@@ -1,6 +1,9 @@
 #include "Calendar.h"
 #include <iostream>
+#include <fstream>
 using namespace std;
+
+const char* DATABASE_FILE_PATH = "database.txt";
 
 void Calendar::resize() {
     capacity *= 2;
@@ -23,9 +26,10 @@ void Calendar::copyFrom(const Calendar& calendar) {
 }
 
 Calendar::Calendar() {
-    capacity = 2;
-    days = new DaySchedule[capacity];
-    size = 0;
+    // capacity = 2;
+    // days = new DaySchedule[capacity];
+    // size = 0;
+    readFromFile();
 }
 Calendar::Calendar(const Calendar& calendar) {
     copyFrom(calendar);
@@ -33,6 +37,7 @@ Calendar::Calendar(const Calendar& calendar) {
 bool Calendar::addDay(const Date& date) {
     if(capacity == size) 
         resize();
+    
     for(int i = 0; i < size; i++) {
         if(days[i].getDate() == date) 
             return false;
@@ -50,7 +55,8 @@ bool Calendar::addEvent(const Event& event) {
     }
     if(dayIndex == size) 
         addDay(event.getDate());
-    return days[size - 1].addEvent(event);
+        
+    return days[dayIndex].addEvent(event);
 }
 bool Calendar::removeEvent(const Event& event) {
     for(int i = 0; i < size; i++) {
@@ -78,13 +84,13 @@ void Calendar::printDay(const Date& date) const {
         cout << *days[index].getEvents()[i] << endl;
     }
 }
-void Calendar::findEventsByString(const String& str) const {
+void Calendar::printEventsByString(const String& str) const {
     for(int i = 0; i < size; i++) {
         const Event** dayEvents = days[i].getEvents();
         for(int j = 0; j < days[i].getSize(); j++) {
             if(dayEvents[j]->getName().contains(str) ||
             dayEvents[j]->getComment().contains(str)) 
-                cout << *dayEvents[j] << endl;
+                (*dayEvents[j]).print();
         }
     }
 }
@@ -201,6 +207,89 @@ int Calendar::getDayIndexByDate(const Date& date) const {
     return -1;
 }
 
+void Calendar::getStatsForPeriodInFile(const Date& date1, const Date& date2) {
+    int countOfEvents[7] = {};
+    for(int i = 0; i < size; i++) {
+        if(((days[i].getDate() >= date1) && (days[i].getDate() <= date2)))
+            countOfEvents[days[i].getDate().getDayOfWeek()] += days[i].getSize();
+    }
+    String filePath = String().concat(date1.getYear()).concat("-").concat(date1.getMonth())
+        .concat("-").concat(date1.getDay()).concat(".txt");
+    cout << filePath.toString() << endl;
+    ofstream file(filePath.toString());
+    for(int i = 0; i < 7; i++) {
+        switch(i) {
+            case 0: file << "Sunday: "; break;
+            case 1: file << "Monday: "; break;
+            case 2: file << "Tuesday: "; break;
+            case 3: file << "Wednesday: "; break;
+            case 4: file << "Thursday: "; break;
+            case 5: file << "Friday: "; break;
+            case 6: file << "Saturday: "; break;
+        }
+        file << countOfEvents[i] << " events" << endl;
+    }
+}
+
+int Calendar::getCountOfEvents() const {
+    int count = 0;
+    for(int i = 0; i < size; i++) {
+        for(int j = 0; j < days[i].getSize(); j++) {
+            count++;
+        }
+    }
+    return count;
+}
+
+void Calendar::saveInFile() const {
+    ofstream file(DATABASE_FILE_PATH);
+    file << size << endl;
+    file << getCountOfEvents() << endl;
+    for(int i = 0; i < size; i++) {
+        for(int j = 0; j < days[i].getSize(); j++) {
+            file << *(days[i].getEvents()[j]) << endl;
+        }
+    }
+}
+
+void Calendar::readFromFile() {
+    ifstream file(DATABASE_FILE_PATH);
+    if(!file.is_open()) return;
+    int countOfDaysInFile = 0;
+    file >> countOfDaysInFile;
+    days = new DaySchedule[countOfDaysInFile];
+    capacity = countOfDaysInFile;
+    size = 0;
+    int countOfEventsInFile = 0;
+    file >> countOfEventsInFile;
+    for(int i = 0; i < countOfEventsInFile; i++) {
+        int nameLength = 0;
+        file >> nameLength;
+        file.ignore();
+        char* name = new char[nameLength + 1];
+        file.getline(name, nameLength + 1);
+        name[nameLength] = '\0';
+        int commentLength = 0;
+        file >> commentLength;
+        file.ignore();
+        char* comment = new char[commentLength + 1];
+        file.getline(comment, commentLength + 1);
+        comment[commentLength] = '\0';
+        int day, month, year;
+        file >> day >> month >> year;
+        Date date(day, month, year);
+        int hours, minutes, seconds;
+        file >> hours >> minutes >> seconds;
+        Time startTime(hours, minutes, seconds);
+        file >> hours >> minutes >> seconds;
+        Time endTime(hours, minutes, seconds);
+        Event event(name, comment, date, startTime, endTime);
+        addEvent(event);
+        delete[] name, comment;
+    }
+}
+
 Calendar::~Calendar() {
+    saveInFile();
     free();
 } 
--- Calendar.h
+++ Calendar.h
@@ -15,18 +15,22 @@ class Calendar {
     void copyFrom(const Calendar&);
     int getDayIndexByDate(const Date&) const;
     bool addDay(const Date&);
+    void saveInFile() const;
+    void readFromFile();
+    int getCountOfEvents() const;
 public:
     Calendar();
     Calendar(const Calendar&);
     bool addEvent(const Event&);
     bool removeEvent(const Event&);
     void printDay(const Date&) const;
-    void findEventsByString(const String&) const;
+    void printEventsByString(const String&) const;
     bool changeDate(const Event&, const Date&);
     bool changeName(const Event&, const String&);
     bool changeComment(const Event&, const String&);
     bool changeStartTime(const Event&, const Time&);
     bool changeEndTime(const Event&, const Time&);
+    void getStatsForPeriodInFile(const Date&, const Date&);
     Calendar& operator=(const Calendar&);
     ~Calendar();   
 };
